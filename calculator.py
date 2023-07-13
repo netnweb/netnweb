a4272db8f73239b33acb13d62c52e262d0dd33b8
netnweb <netnweb@MINJUNui-MacBookAir.local>
2023-07-13 16:50:05 +0900

    first commit

---
+++ calculator.py
@@ -0,0 +1,29 @@
+
+    print("select operationo to perform by entering number ")
+    print("1.addition")
+    print("2.subtraction")
+    print("3.multiplication")
+    print("4.division")
+
+    operation = input()
+    if operation == 1:
+        num1 = ("enter number 1"):
+        num2 = ("enter number 2"):
+        print (" The sum is " + num1+num2);
+    if operation == 2:
+        num1 = ("enter number 1"):
+        num2 = ("enter number 2"):
+        print ("The sum is" + num1-num2);
+    if operation == 3:
+        num1 = ("enter number 1"):
+        num2 = ("enter number 2"):
+        print ("The sum is" +num1*num2);
+    else if operation == 4:
+         num1 = ("enter number 1"):
+         num2 = ("enter number 2"):
+         print ("The sum is " + num1/num2);
+
+
+
+
+
